ab4930caa1794880509824da67fc14d59868dca5
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2025-02-23 17:24:10 +0530

    added some ui components

--- src/components/ui/accordian.tsx
+++ src/components/ui/accordian.tsx
@@ -1,12 +1,13 @@
-"use client" 
 
-import * as React from "react";
-import * as AccordianPrimitive from "@radix-ui/react-accordion"
-import {ChevronDown} from "lucide-react"
+"use client"
 
-import {cn} from "@lib/utils"
+import * as React from "react"
+import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { ChevronDown } from "lucide-react"
 
-const Accordian = AccordianPrimitive.Root
+import { cn } from "../../app/lib/utils"
+
+const Accordion = AccordionPrimitive.Root
 
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
@@ -32,3 +33,27 @@ const AccordionTrigger = React.forwardRef<
         className
       )}
       {...props}
+    >
+      {children}
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+))
+AccordionTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const AccordionContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    {...props}
+  >
+    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+  </AccordionPrimitive.Content>
+))
+
+AccordionContent.displayName = AccordionPrimitive.Content.displayName
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
